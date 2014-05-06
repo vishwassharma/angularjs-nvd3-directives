cf42d32f517e47d8b826aef8e51f1cb3d9baa8bc
Vishwas Sharma <vishwasinventor@gmail.com>
2014-05-06 18:07:53 +0530

    added structure for nvd3 multichart

--- src/directives/nvd3Directives.js
+++ src/directives/nvd3Directives.js
@@ -2237,7 +2237,154 @@
                     }, (attrs.objectequality === undefined ? false : (attrs.objectequality === 'true')));
                 }
             };
-        }]);
+        }])
+    .directive('nvd3MultiChart', [function(){
+        return {
+            restrict: 'EA',
+            scope: {
+                data: '=',
+                width: '@',
+                height: '@',
+                id: '@',
+                showlegend: '@',
+                tooltips: '@',
+                showxaxis: '@',
+                showyaxis: '@',
+                rightalignyaxis: '@',
+                defaultstate: '@',
+                nodata: '@',
+                margin: '&',
+                tooltipcontent: '&',
+                color: '&',
+                x: '&',
+                y: '&',
+                forcex: '@',
+                forcey: '@',
+                isArea: '@',
+                interactive: '@',
+                clipedge: '@',
+                clipvoronoi: '@',
+                interpolate: '@',
+
+                callback: '&',
+
+                useinteractiveguideline: '@',
+                //xaxis
+                xaxisorient: '&',
+                xaxisticks: '@',
+                xaxistickvalues: '&xaxistickvalues',
+                xaxisticksubdivide: '&',
+                xaxisticksize: '&',
+                xaxistickpadding: '&',
+                xaxistickformat: '&',
+                xaxislabel: '@',
+                xaxisscale: '&',
+                xaxisdomain: '&',
+                xaxisrange: '&',
+                xaxisrangeband: '&',
+                xaxisrangebands: '&',
+                xaxisshowmaxmin: '@',
+                xaxishighlightzero: '@',
+                xaxisrotatelabels: '@',
+                xaxisrotateylabel: '@',
+                xaxisstaggerlabels: '@',
+                xaxislabeldistance: '@',
+
+                //yaxis
+                yaxisorient: '&',
+                yaxisticks: '&',
+                yaxistickvalues: '&yaxistickvalues',
+                yaxisticksubdivide: '&',
+                yaxisticksize: '&',
+                yaxistickpadding: '&',
+                yaxistickformat: '&',
+                yaxislabel: '@',
+                yaxisscale: '&',
+                yaxisdomain: '&',
+                yaxisrange: '&',
+                yaxisrangeband: '&',
+                yaxisrangebands: '&',
+                yaxisshowmaxmin: '@',
+                yaxishighlightzero: '@',
+                yaxisrotatelabels: '@',
+                yaxisrotateylabel: '@',
+                yaxisstaggerlabels: '@',
+                yaxislabeldistance: '@',
+
+                legendmargin: '&',
+                legendwidth: '@',
+                legendheight: '@',
+                legendkey: '@',
+                legendcolor: '&',
+                legendalign: '@',
+                legendrightalign: '@',
+                legendupdatestate: '@',
+                legendradiobuttonmode: '@',
+
+                //angularjs specific
+                objectequality: '@',  //$watch(watchExpression, listener, objectEquality)
+
+                //d3.js specific
+                transitionduration: '@'
+
+            },
+            controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs){
+                $scope.d3Call = function(data, chart){
+                    checkElementID($scope, $attrs, $element, chart, data);
+                };
+            }],
+            link: function(scope, element, attrs){
+                scope.$watch('data', function(data){
+                    if(data){
+                        //if the chart exists on the scope, do not call addGraph again, update data and call the chart.
+                        if(scope.chart){
+                            return scope.d3Call(data, scope.chart);
+                        }
+                        nv.addGraph({
+                            generate: function(){
+                                initializeMargin(scope, attrs);
+                                var chart = nv.models.multiChart()
+                                    .width(scope.width)
+                                    .height(scope.height)
+                                    .margin(scope.margin)
+                                    .x(attrs.x === undefined ? function(d){ return d[0]; } : scope.x())
+                                    .y(attrs.y === undefined ? function(d){ return d[1]; } : scope.y())
+                                    .forceX(attrs.forcex === undefined ? [] : scope.$eval(attrs.forcex)) // List of numbers to Force into the X scale (ie. 0, or a max / min, etc.)
+                                    .forceY(attrs.forcey === undefined ? [0] : scope.$eval(attrs.forcey)) // List of numbers to Force into the Y scale
+                                    .showLegend(attrs.showlegend === undefined ? false : (attrs.showlegend === 'true'))
+                                    .tooltips(attrs.tooltips === undefined ? false : (attrs.tooltips  === 'true'))
+                                    .showXAxis(attrs.showxaxis === undefined ? false : (attrs.showxaxis  === 'true'))
+                                    .showYAxis(attrs.showyaxis === undefined ? false : (attrs.showyaxis  === 'true'))
+                                    .rightAlignYAxis(attrs.rightalignyaxis === undefined ? false : (attrs.rightalignyaxis  === 'true'))
+                                    .noData(attrs.nodata === undefined ? 'No Data Available.' : scope.nodata)
+                                    .interactive(attrs.interactive === undefined ? false : (attrs.interactive === 'true'))
+                                    .clipEdge(attrs.clipedge === undefined ? false : (attrs.clipedge === 'true'))
+                                    .clipVoronoi(attrs.clipvoronoi === undefined ? false : (attrs.clipvoronoi === 'true'))
+                                    .interpolate(attrs.interpolate === undefined ? 'linear' : attrs.interpolate)
+                                    .color(attrs.color === undefined ? nv.utils.defaultColor()  : scope.color())
+                                    .isArea(attrs.isarea === undefined ? function(d) { return d.area; } : function(){ return (attrs.isarea === 'true'); });
+
+                                if (chart.useInteractiveGuideline) {
+                                    chart.useInteractiveGuideline(attrs.useinteractiveguideline === undefined ? false : (attrs.useinteractiveguideline === 'true'));
+                                }
+
+                                if(attrs.tooltipcontent){
+                                    chart.tooltipContent(scope.tooltipcontent());
+                                }
+
+                                scope.d3Call(data, chart);
+                                nv.utils.windowResize(chart.update);
+                                scope.chart = chart;
+                                return chart;
+                            },
+                            callback: attrs.callback === undefined ? null : scope.callback()
+                        });
+                    }
+                }, (attrs.objectequality === undefined ? false : (attrs.objectequality === 'true')));
+            }
+        };
+    }])
+    ;
 
     //still need to implement
     //sparkbars??
